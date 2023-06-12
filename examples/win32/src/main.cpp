f80f45c530d014c6132e97d64b4ab9f10b284638
suddenly <suddenlybydl0@gmail.com>
2023-06-12 19:28:18 +0500

    🥴

--- examples/win32/src/main.cpp
+++ examples/win32/src/main.cpp
@@ -1,16 +1,41 @@
-﻿#include <windows.h>
+﻿import null.sdk;
 import null.input;
-import null.sdk;
 
-utils::win::c_window window{ };
-null::input::c_key& mouse_left = null::input::keys[null::e_key_id::mouse_left];
+//@note: we need an example proxy file for the correct order of initialization of variables.
+//       For msvc, 3 years after the adoption of the 20th standard, the compiler is not able to cope with the code generation of modules,
+//       as a result of which we first initialize the variables of our example, and only then the variables in the library, which can cause a crash
+import example;
+
+#include <windows.h>
+#include <iostream>
+
+        null::input::event_dispatcher.detach_listener(null::input::e_event_type::key_down, this);
+    time_measurement.begin();
+        null::input::event_dispatcher.detach_listener(null::input::e_event_type::key_up, this);
+    }
+    mouse_left.callbacks.at<null::input::e_key_callbacks::on_released>().add([] { std::cout << "mouse_left released" << std::endl; });
+    mouse_left.callbacks.at<null::input::e_key_callbacks::on_down>().add([] { std::cout << "mouse_left down" << std::endl; });
+public:
+    window.callbacks.at<utils::win::e_window_callbacks::on_main_loop>().add(main_loop);
+    window.callbacks.at<utils::win::e_window_callbacks::on_wnd_proc>().add(null::input::wnd_proc);
+        std::cout << key.data.name << " key down" << std::endl;
+    }
+
+        window.main_loop();
+        std::cout << key.data.name << " key up" << std::endl;
+    } catch(const std::exception& exception) {
+        std::cout << exception.what() << std::endl;
 
 void main_loop() {
-    null::input::begin_frame(window);
+    time_measurement.update();
+    null::input::begin_frame(time_measurement);
 
-    if(mouse_left.is_down()) {
+    if(mouse_left.is_down())
         std::cout << "mouse_left down" << std::endl;
-    }
+
+    //@note: null::input::keys[null::input::e_key_id::mouse_left].is_down() && null::input::keys[null::input::e_key_id::mouse_right].is_down()
+    if(null::input::keys_view_t{ null::input::e_key_id::mouse_left, null::input::e_key_id::mouse_right }.is_down())
+        std::cout << "mouse_left and mouse_right down" << std::endl;
 }
 
 int main(HINSTANCE instance) {
@@ -27,6 +52,4 @@ int main(HINSTANCE instance) {
     } catch(std::exception exp) {
         std::cout << exp.what() << std::endl;
     }
-
-    return 0;
 }
