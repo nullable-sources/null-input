9c7d856b51210b5dd89e0ee08205ab0368594763
suddenly <suddenlybydl0@gmail.com>
2023-06-12 19:28:26 +0500

    🥴

--- examples/win32/src/main.cpp
+++ examples/win32/src/main.cpp
@@ -9,41 +9,14 @@ import example;
 #include <windows.h>
 #include <iostream>
 
-        null::input::event_dispatcher.detach_listener(null::input::e_event_type::key_down, this);
-    time_measurement.begin();
-        null::input::event_dispatcher.detach_listener(null::input::e_event_type::key_up, this);
-    }
-    mouse_left.callbacks.at<null::input::e_key_callbacks::on_released>().add([] { std::cout << "mouse_left released" << std::endl; });
-    mouse_left.callbacks.at<null::input::e_key_callbacks::on_down>().add([] { std::cout << "mouse_left down" << std::endl; });
-public:
-    window.callbacks.at<utils::win::e_window_callbacks::on_main_loop>().add(main_loop);
-    window.callbacks.at<utils::win::e_window_callbacks::on_wnd_proc>().add(null::input::wnd_proc);
-        std::cout << key.data.name << " key down" << std::endl;
-    }
-
-        window.main_loop();
-        std::cout << key.data.name << " key up" << std::endl;
-    } catch(const std::exception& exception) {
-        std::cout << exception.what() << std::endl;
-
-void main_loop() {
-    time_measurement.update();
-    null::input::begin_frame(time_measurement);
-
-    if(mouse_left.is_down())
-        std::cout << "mouse_left down" << std::endl;
-
-    //@note: null::input::keys[null::input::e_key_id::mouse_left].is_down() && null::input::keys[null::input::e_key_id::mouse_right].is_down()
-    if(null::input::keys_view_t{ null::input::e_key_id::mouse_left, null::input::e_key_id::mouse_right }.is_down())
-        std::cout << "mouse_left and mouse_right down" << std::endl;
-}
-
 int main(HINSTANCE instance) {
     window = utils::win::c_window{ instance };
 
-    mouse_left.callbacks.add<void()>(null::e_key_state::released, []() { std::cout << "mouse_left released" << std::endl; });
+    mouse_left.callbacks.at<null::input::e_key_callbacks::on_released>().add([] { std::cout << "mouse_left released" << std::endl; });
+    mouse_left.callbacks.at<null::input::e_key_callbacks::on_down>().add([] { std::cout << "mouse_left down" << std::endl; });
 
-    window.callbacks.set<bool(HWND, UINT, WPARAM, LPARAM)>(utils::e_window_callbacks::wnd_proc, null::input::wnd_proc);
+    window.callbacks.at<utils::win::e_window_callbacks::on_main_loop>().add(main_loop);
+    window.callbacks.at<utils::win::e_window_callbacks::on_wnd_proc>().add(null::input::wnd_proc);
 
     try {
         window.create();
